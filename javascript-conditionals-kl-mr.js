cfb311ce041d4ba0948fc9b64266a7e4edee0779
rhodesmt <mrhodes415@gmail.com>
2022-09-08 15:01:05 -0700

    2nd Question Answered

---
+++ javascript-conditionals-kl-mr.js
@@ -0,0 +1,22 @@
+let item1 = 50
+
+if(item1 <= 100){
+    console.log("In Budget")
+} else {
+    console.log("Not In Budget")
+}
+
+let feeling1 = "Hungry"
+let feeling2 = "Not Hungry"
+
+if (feeling1) {
+    console.log("Eat Food")
+} else {
+    console.log("Keep Coding")
+}
+
+if (feeling2) {
+    console.log("Keep Coding")
+} else {
+    console.log("Eat Food")
+} 
