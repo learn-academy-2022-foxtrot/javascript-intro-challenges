b5ec6139a441e1886886681e65b01bbd3b163688
ZackRohde <zdrohde@outlook.com>
2022-09-08 11:42:33 -0700

    Challenge

---
+++ javascript-zack-lyssa.js
@@ -0,0 +1,22 @@
+console.log(34 + 71)
+
+console.log(67 - 123)
+
+console.log(56 * 23)
+
+console.log(45 / 5)
+
+console.log(5**7)
+
+console.log(33 % 6 )
+
+let student = "zack"
+console.log(student.length)
+
+console.log(student.includes("e"))
+
+console.log(student[0])
+
+console.log(student.toUpperCase())
+
+
