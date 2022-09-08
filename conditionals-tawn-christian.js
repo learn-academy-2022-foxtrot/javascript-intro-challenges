4883ab2c47f67ed2fa35301560ecea4bdce7a17c
c.r.macias56@hotmail.com <c.r.macias56@hotmail.com>
2022-09-08 16:28:06 -0700

    finished 5

--- conditionals-tawn-christian.js
+++ conditionals-tawn-christian.js
@@ -25,16 +25,32 @@
 //     console.log("stop")
 // }
 
-var trafficLight = "Yellow"
+// var trafficLight = "Yellow"
 
-if(trafficLight === "Green") {
-    console.log("Go man!!!")
-} else if(trafficLight === "Yellow") {
-    console.log("Slow down or Speed up!")
-} else {
-    console.log("Stop!!!")
-}
+// if(trafficLight === "Green") {
+//     console.log("Go man!!!")
+// } else if(trafficLight === "Yellow") {
+//     console.log("Slow down or Speed up!")
+// } else {
+//     console.log("Stop!!!")
+// }
 
 // Write a statement that takes two variables that are numbers and outputs the larger number. If the numbers are equal, output "the numbers are the same".
+var myNum1 = 10
+var myNum2 = 10
 
+if (myNum1 < myNum2){
+console.log(myNum2)
+} else if (myNum1 > myNum2){
+    console.log(myNum1)
+} else {
+ console.log("the numbers are the same")
+}
 // Write a statement that takes a variable of a number and logs whether the number is odd, even, or zero.
+var number = 8
+
+if (number % 2 == 0){
+ console.log("this can be split")
+} else {
+    console.log("this is odd")
+} 
