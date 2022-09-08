f9e72713fa585d2e1613850a92fc7a19dbcd6f1c
c.r.macias56@hotmail.com <c.r.macias56@hotmail.com>
2022-09-08 15:19:59 -0700

    another try

--- conditionals-tawn-christian.js
+++ conditionals-tawn-christian.js
@@ -1,9 +1,29 @@
 // Write a statement that takes a variable of item and logs "in budget" if a price is $100 or less.
+// var number = 9 
 
+// if (number < 100){
+//     console.log("in budget")
+// } 
 
 // Write a statement that takes a variable of hungry and logs "eat food" if you are hungry and "keep coding" if you are not hungry.
+// var hungry = false
+
+// if (hungry === true){
+//     console.log("eat food")
+// } else {
+//     console.log("keep coding")
+// }
 
 // Write a statement that takes a variable of trafficLight and logs "go" if the light is green, "slow down" if the light is yellow and "stop" if the light is red.
+var trafficLight = yellow
+
+if (trafficLight = green){
+    console.log("go")
+} else if (trafficLight = yellow){
+    console.log("slow down")
+} else {
+    console.log("stop")
+}
 
 // Write a statement that takes two variables that are numbers and outputs the larger number. If the numbers are equal, output "the numbers are the same".
 
