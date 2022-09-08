68c9c6bed7f30edece100089fe7ad1b6f875765c
nathaniel-ross <nathaniel.rossloyo@gmail.com>
2022-09-08 16:28:39 -0700

    finished 2 stretch challenges

--- conditionals-nate-olena.js
+++ conditionals-nate-olena.js
@@ -1,7 +1,7 @@
 //Write a statement that takes a variable of item and logs "in budget" if a price is $100 or less.
 let price = "$100"
 if (price === "$100" || price < "$100") {
-    console.log("You are in buget")
+    console.log("You are in budget")
 }
 
 //Write a statement that takes a variable of hungry and logs "eat food" if you are hungry and "keep coding" if you are not hungry.
@@ -39,4 +39,39 @@ if (num === 0) {
     console.log("the number is even")
 } else {
     console.log("the number is odd")
+}
+
+// Write a statement that takes a variable of a grade percentage and logs the letter grade for that percentage, if the grade is 100% log "perfect score", if the grade is zero log "no grade available."
+
+let grade = 0
+if (grade === 100) {
+    console.log("perfect score") 
+} else if (grade < 100 && grade > 89) {
+    console.log("You got an A") 
+} else if (grade < 90 && grade > 79) {
+    console.log("You got a B")
+} else if (grade < 80 && grade > 69) {
+    console.log("You got a C")
+} else if (grade < 70 && grade > 59) {
+    console.log("You got a D")
+} else if (grade < 60 && grade > 0) {
+    console.log("You got an F")
+} else if (grade === 0) {
+    console.log("no grade available")
+}
+
+// Write a statement that takes a variable of a boolean, number, or string data type and logs the data type of the variable. HINT: Check out the JavaScript typeof operator.
+
+let boolean = true
+let number = 15
+let string = "hi"
+console.log(typeof true)
+console.log(typeof 15)
+console.log(typeof "hi")
+
+// Create a password checker using a single conditional statement. If a user inputs a password with 12 or more characters AND the password includes !, then log "That is a mighty strong password!" If the user’s password is 8 or more characters OR includes !, then log "That password is strong enough." Log "That is not a valid password." for every other input.
+
+let password = "password12345!"
+if (password.length == 12 && password.length > 12) {
+    console.log("That is a mighty strong password!")
 }
