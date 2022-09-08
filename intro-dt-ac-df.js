88633c13e523f8eba1150d28149111078c234f41
LEARN Academy <learnacademy@LEARNs-MacBook-Air.local>
2022-09-08 11:58:43 -0700

    practiceThing

--- intro-dt-ac-df.js
+++ intro-dt-ac-df.js
@@ -12,8 +12,11 @@ console.log(56*23)
 console.log(45/5)
 
 // Write the code that will log the outcome of 5 to the power of 7.
+console.log(5**7)
 // Write the code that will log the whole number remainder of 33 divided by 6.
+console.log(33%6)
 // Write the code that will log the length of a string containing your name.
+console.log("athian")
 // Write the code that will log whether your string includes the letter "e"?
 // Write the code that will log the character at the first index of the string.
 // Write the code that will log the string in all uppercase letters.
