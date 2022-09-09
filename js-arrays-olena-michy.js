c984586a0721e73f3a94a0c214bc245c081969a3
LEARN Academy <learnacademy@LEARNs-MacBook-Air.local>
2022-09-09 15:37:07 -0700

    1st task completed

---
+++ js-arrays-olena-michy.js
@@ -0,0 +1,20 @@
+// Write the code that will add "soda" to the end of the original array. 
+// node js-arrays-olena-michy.js
+
+/*let groceryList = ["chips", "dip", "cookies"]
+console.log(groceryList.push("soda"))
+console.log(groceryList)*/
+
+// Write the code that will add "granola" to the end of array without altering the original array.
+
+/*et groceryList = ["chips", "dip", "cookies"]
+console.log(groceryList.push("granola"))
+console.log(groceryList)*/
+
+// Write the code that will return a subset of the array containing only "chips" and "dip".
+
+let groceryList = ["chips", "dip", "cookies"]
+console.log(groceryList.slice(0, 2))
+
+
+
