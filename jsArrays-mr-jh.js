056f89bbfcbd20b35c44bf934160674d5e06e52a
Joseph <jhan.technology@gmail.com>
2022-09-09 12:01:57 -0700

    Completed bulk of questions

--- jsArrays-mr-jh.js
+++ jsArrays-mr-jh.js
@@ -5,44 +5,81 @@
 
  let groceryList = ["chips", "dip", "cookies"];
 // Write the code that will add "soda" to the end of the original array.
-    groceryList.push('soda')
-    console.log(groceryList)
+    // groceryList.push('soda')
+    // console.log(groceryList)
 
 // Write the code that will add "granola" to the end of array without altering the original array.
-    let tempArr = groceryList
-    tempArr.push('granola')
-    console.log(tempArr)
+    // groceryList.push('granola')
+    // console.log(groceryList)
 
 // Write the code that will return a subset of the array containing only "chips" and "dip".
+    // console.log(groceryList[0], groceryList[1])
+
 
 // Write the code that will add "beans" to the "chips" and "dip" array.
+    
+    // Different method of solving.
+        // groceryList[2] = 'beans'
+        // console.log(groceryList[0], groceryList[1], groceryList[2])
+
+    // groceryList.pop('cookies')
+    // groceryList.push('beans')
+    // console.log(groceryList)
+
 
 // Consider the variable:
 
-// var numbers = [2, 4, 6, 8, 10];
+let numbers = [2, 4, 6, 8, 10];
 // Write the code that will add the number 0 to the beginning of the array.
+    // numbers.unshift(0)
+    // console.log(numbers)
+
 
 // Write the code that will add the number 12 to the end of the array.
+    // numbers.push(12)
+    // console.log(numbers)
+
 
 // Write the code that will remove the first number from the array.
+    // numbers.shift(0)
+    // console.log(numbers)
+
 
 // Write the code that will add the number 0 to the beginning of the array without altering the original array. HINT: it's not .unshift You'll have to get creative! ;)
+    // let zeroHolderArr = [0]
+    // let arrCopy = numbers.slice(0)    
+    // // [2, 4, 6, 8, 10] ==> [2, 4, 6, 8, 10]
+
+    // console.log(zeroHolderArr.concat(arrCopy))
+
+
 
 // Consider the variable:
 
-// var numSet = [2, 13, 6, 8, 4, 2];
+let numSet = [2, 13, 6, 8, 4, 2];
 // Write the code that finds the index of the first appearance of the number 2.
+    // console.log(numSet.indexOf(2))
+
 
 // Write the code that finds the index of the last appearance of the number 2.
+    // console.log(numSet.lastIndexOf(2))
+
 
 // Write the code that returns the number at the third index.
+    // console.log(numSet[3])
+
 
 // Consider the variable:
 
-// var characters = ["y", "a", "r", "r", "a"];
+let characters = ["y", "a", "r", "r", "a"];
 // Write the code that brings all the letters in the characters array together into a string.
+    // console.log(characters.join(''))
+
 
 // Write the code that reverses the order of the letters in the characters array and saves it into a variable called charsReversed.
+    // let charsReversed = characters.reverse()
+    // console.log(charsReversed)
+
 
 // Write the code that brings all the letters in the charsReversed array together into a string with an asterisk between each letter.
 
