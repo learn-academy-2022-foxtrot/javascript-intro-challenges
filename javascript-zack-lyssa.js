62e707f3a8f9c0c9c1067d716eb81e5848bd8e74
aribrashear <aribrashear@gmail.com>
2022-09-08 17:10:01 -0700

    Downloaded fresh repository, copied relevant files, uploading to attempt merge again.

---
+++ javascript-zack-lyssa.js
@@ -0,0 +1,54 @@
+console.log(34 + 71)
+
+console.log(67 - 123)
+
+console.log(56 * 23)
+
+console.log(45 / 5)
+
+console.log(5**7)
+
+console.log(33 % 6 )
+
+let student = "zack"
+console.log(student.length)
+
+console.log(student.includes("e"))
+
+console.log(student[0])
+
+console.log(student.toUpperCase())
+
+console.log(34 / 3 >= 67 / 2)
+
+console.log(5 == "5")
+
+console.log(5 === "5")
+
+console.log(3 !== 3)
+
+console.log("LEARN".length === 5 && "Student".length === 7)
+
+console.log("LEARN".length === 5 || "Student".length === 10)
+
+console.log("LEARN".includes("RN"))
+
+console.log("LEARN".includes("rn"))
+
+console.log("LEARN"[0] === "l")
+
+console.log("LEARN"[0] ==="L")
+
+var theQuestion = "life, the universe, and everything";
+var theAnswer = 42;
+console.log(theAnswer / 2)
+
+console.log(theQuestion.length)
+
+console.log(theQuestion + theAnswer)
+
+console.log("the universe")
+
+console.log(theQuestion.charAt(0))
+
+console.log(theQuestion.length > theAnswer)
