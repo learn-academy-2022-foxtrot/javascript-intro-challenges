bdb8237c7a9a71930e7f5c854c81fc8e285964f6
Heath-H <heathhaupt92@gmail.com>
2022-09-12 11:42:47 -0700

    loop changes

--- loops-kyle-heath-nico.js
+++ loops-kyle-heath-nico.js
@@ -1,3 +1,12 @@
 for(let i=0; i<21; i++) {
     console.log(i)
 }
+
+for(let i=1; i<21; i+=2) {
+    console.log(i)
+}
+
+var array = [1, 2, 3, 4, 5, 6 ,7 ,8 ,9 ,10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+for(let i=0; i<array.length; i++) {
+    console.log(array[i] * 3)
+}
