8d1bd5d068f90a678eb36f6f348651376ec8194c
nouxcode <perrault.meaghan@gmail.com>
2022-09-12 13:01:22 -0700

    loops challenges update, lunch break

--- loops-kyle-heath-nico.js
+++ loops-kyle-heath-nico.js
@@ -10,3 +10,12 @@ var array = [1, 2, 3, 4, 5, 6 ,7 ,8 ,9 ,10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 
 for(let i=0; i<array.length; i++) {
     console.log(array[i] * 3)
 }
+
+
+const numsArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+for(let i=0; i<numsArray.length; i++){
+    if(numsArray[i]% 2 !== 0){
+      console.log(`${numsArray}`)  
+    }
+    
+}
