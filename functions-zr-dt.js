baffbf13a26888aa8d9e7895d5c586221489fb61
dthomas91 <davidthomas8891@gmail.com>
2022-09-12 16:21:22 -0700

    Practice

---
+++ functions-zr-dt.js
@@ -0,0 +1,39 @@
+// const marco = () => {
+//     return "polo"
+//     } 
+//     console.log(marco())
+
+// const greeting = (name) => {
+
+//     return `Welcome, ${name}`
+// }
+// console.log(greeting("David"))
+
+// const oddOrEven = (num1) => {
+//   if (num1 % 2 !== 0) {
+//     return (`${num1} is odd`)
+//   } else if (num1 % 2 == 0) {
+//     return (`${num1} is even`) 
+//   } 
+// }     
+// console.log(oddOrEven(168))
+
+// const triple = (num) => {
+//   return num * 3
+// }
+// console.log(triple(5))
+
+// const assignGrade = (num) => {
+//   if(num == "100") { 
+//     return  `${num} = A` 
+//   } else if (num >= 80 && num <= 91) {
+//     return `${num} = B`
+//   } else {
+//     return "Study more"
+//   }
+  
+// }
+// console.log(assignGrade(79))
+
+  
+
