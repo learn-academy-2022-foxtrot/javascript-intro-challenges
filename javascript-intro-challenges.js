2fc7eeb3cbb461c2cb7f20c2c14267469dc695f8
LEARN Academy <learnacademy@LEARNs-MacBook-Air.local>
2022-09-12 16:42:01 -0700

    functionPractice

---
+++ javascript-intro-challenges.js
@@ -0,0 +1,83 @@
+// Write a function named marco that returns "polo".
+
+// const marco = () => { 
+//     return "polo"
+// }
+// console.log(marco())
+
+
+// Write a function named greeting that takes a name as an argument and returns "Welcome, <person's name here>!"
+
+// const greeting = (name) => {
+//     return `welcome ${name} !`
+// }
+// console.log(greeting("athian"))
+
+// // Write a function named oddOrEven that takes a number as an argument and returns whether the number is odd or even.
+
+// const oddOrEven = (num0) => {
+//     if (num0 % 2 ===0) {
+//         return  "even"
+//     } else if (num0 % 2 !==0){
+//         return "odd"
+//     } else {
+//         return "is that a letter"
+//     }
+   
+// }
+// console.log(oddOrEven(4))
+
+// Write a function named triple that takes a number as an argument and returns the result of that number multiplied by 3.
+
+// var numThree = 3
+// const triple =(num1, numThree) => {
+//     return num1 * numThree 
+
+//     }
+
+// console.log(triple(5, numThree))
+
+// Write a function named multiply that takes two numbers as arguments and returns the result of the numbers multiplied together.
+
+
+// const multiply = (num1, num2) => {
+//     return  num1 * num2
+// }
+// console.log(multiply (5, 5))
+
+// Write a function named divisibleBy that takes two numbers as arguments and returns whether the first number is evenly divisible by the second so that divisibleBy(10, 5) logs "10 is evenly divisible by 5".
+
+// const divisibleBy = (num7, num8) => {
+//  if(num7 % num8 ===0){
+//     return "your first number is divisible by your second number"
+//  } else {
+//     return "they dont divide evenly"
+//  }
+// }
+// console.log(divisibleBy(10,5))
+
+// Write a function named assignGrade that takes a number score as an argument and returns the letter grade for the score.
+
+// Write a function named isLonger that takes two strings as arguments and returns the string that contains the most characters.
+
+const biggerString =(string1, string2) => {
+    if(string1.length> string2.length) {
+        return string1
+    } else if (string1.length< string2)
+
+    }
+}
+
+// Write a function named greaterNum that takes two numbers as arguments and returns whichever number is the greater (higher) number.
+
+// -. Write a function named yelling that takes a string as an argument and return the string in all uppercase case letters.
+
+// 🏔 Stretch Goals
+// The World Translator
+
+// Write a function named helloWorld that takes a language code (e.g. "es", "de", "en") as an argument and returns "Hello World!" in the given language. Ensure you function works for at least 5 languages.
+// Have your function default to returning English.
+// The Pluralizer
+
+// Write a function named pluralizer that takes a number and a singular noun as arguments and returns the number and pluralized form of the noun, if necessary.
+// Enhance your function so it can handle a few collective nouns like "sheep", "goose", "child", "person" and "species"
