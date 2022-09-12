197c14a10fa52a64574a3bb776184c190cbaf8d8
guanhchan <guanh.chan@gmail.com>
2022-09-12 11:59:38 -0700

    updated_fizzBuzz

--- loops_GC_JH.js
+++ loops_GC_JH.js
@@ -121,13 +121,14 @@ for (i = 5; i < 16;i ++){
 
 for (i = 1; i < 100; i ++){
     console.log(i)
-    if(i % 3 === 0){
+    if (i % 3 === 0 && i % 5 === 0){
+
+        console.log('fizzbuzz')
+    }
+    else if(i % 3 === 0){
         console.log('fizz')
     } else if (i % 5 ===0 ){
         console.log('buzz')
-    } else if (i % 3 === 0 && i % 5 === 0){
-
-        console.log('fizzbuzz')
     }
 
 }
