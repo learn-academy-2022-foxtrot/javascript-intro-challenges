2d29b5561a8c7ea2ead7dd3d08fb9ee0c763f013
c.r.macias56@hotmail.com <c.r.macias56@hotmail.com>
2022-09-13 11:37:38 -0700

    1-3 done

--- func-loops-arrays-ari-christian.js
+++ func-loops-arrays-ari-christian.js
@@ -2,15 +2,65 @@
 // Don't forget to pseudo code.
 
 // 1. Write a function that takes in an array of numbers and returns an array with all numbers multiplied by 3.
+// create a function that returns the value of each element *3
+// input- array of numbers
+// output- array of numbers - collection of results of each element *3
+//step 1. create a function that takes an array as a parameter
+//step 2. declare a variable that is an empty array to store output of function
+//step3. access each element in the array with a for loop
+// step4. manipulate each elemt in the array *5
+//step5. return the new array
 
-// const testArr1 = [3, 9, 15, 4, 10]
-// // output: [9, 27, 45, 12, 30]
+//  const testArr1 = [3, 9, 15, 4, 10]
+// // // output: [9, 27, 45, 12, 30]
 
-// 2. Write a function that takes in an array of numbers and returns a new array with only odd numbers.
-// const testArr2 = [0, 2, -7, 3, 5, 8, 10, 13]
-// // output: [-7, 3, 5, 13]
+// const mult3 = (array) => {
+//     let newArr = []
+//     for(let i=0; i<array.length; i++){
+//         newArr.push(array[i] * 3)
+//     }
+//     return newArr
+// }
+// console.log(mult3(testArr1))
+
+
+
+// 2. Write a function that takes in an array of numbers and returns a new array with only odd numbers
+/**
+ step1. create a function that will accept an array to check each element to determine if its even or odd
+ step2. input - array of numbers\
+ step3 output - array of numbers - only odd numbers
+ step4. create an arrow fucntion named oddFinder that accepts a parameter of array
+ step5. create and empty array to store the values of the new array that will only hold odd numbers
+ step6. use for loop to access every index of the array
+ step7. use conditional statement determine if the numbers are even or odd
+ step8. push all odd numbers to the new array
+ step9. return the new array
+  */
+
+//  const testArr2 = [0, 2, -7, 3, 5, 8, 10, 13]
+// // // output: [-7, 3, 5, 13]
+// const oddFinder = (array) => {
+//     let oddNum = []
+//     for(let i=0; i<array.length; i++){
+//         if(array[i] % 2 !==0) {
+//             oddNum.push(array[i])
+//         }
+//     }
+//     return oddNum 
+// } 
+// console.log(oddFinder(testArr2))
+
 
 // 3. Write a function that takes in an array of numbers and letters and returns a string with only the letters. HINT: use the typeof method.
+/**
+ * step1. create a function called stringInterpolator that will accept parameter of a mixed array
+ * step2. create empty array to stor the value of the new string
+ * step3. Use for loop to look at type of every element 
+ * step4. use conditional statement to determine if the element is a string
+ * step5. return the array full of strings and join it into a single string
+ */
+
 // const comboArr = [
 //   7,
 //   "n",
@@ -28,7 +78,18 @@
 //   false,
 //   "k"
 // ]
-// // output: "nicework"
+// // // output: "nicework"
+
+// const stringInterpolator = (array) => {
+//     let strings = []
+//     for(let i=0; i<array.length; i++){
+//         if(typeof array[i] === typeof "string"){
+//             strings.push(array[i]) 
+//         }
+//     }
+//     return strings.join("")
+// }
+// console.log(stringInterpolator(comboArr))
 
 // 4. Create a function that takes in an array of numbers and returns the sum.
 // const addThese1 = [1, 2, 3, 4]
