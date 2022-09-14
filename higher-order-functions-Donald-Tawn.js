9a83077adeeaf13ba8e54ad1e39ec099bfc480a6
tawnchan <tawnchan@gmail.com>
2022-09-14 08:41:01 -0700

    completed second challenge

--- higher-order-functions-Donald-Tawn.js
+++ higher-order-functions-Donald-Tawn.js
@@ -14,7 +14,7 @@ const mult10 = (array) => {  // declare a function variable and map function str
     return array.map((value) => value * 10) // return the value of each element in the given array multiplied by 10.
 }
 
-console.log(mult10(arr1)) // console.log to test with given arra
+console.log(mult10(arr1)) // console.log to test with given array
 // output: [30, 90, 150, 40, 100]
 
 
@@ -22,9 +22,24 @@ console.log(mult10(arr1)) // console.log to test with given arra
 
 // step 1. declare a function variable
 // step 2. map function structure
-// step 3. return 
+// step 3. create empty array called newArr to contain the new elements retrieved from iteration
+// step 4. map the method, we want the computer to take every element in the array and modulo 2. Since we want Odd numbers, we want to formulate it so that the method pushes any value that is odd into the empty array newArr we created earlier.
+// step 5. return newArr
+// step 6. console log and invoke the function by calling it and applying to whichever array we want, in this case the given array.  console.log(onlyOdd(arr2))
 
-const arr2 = [2, 7, 3, 5, 8, 10, 13]
+const arr2 = [2, 7, 3, 5, 8, 10, 13] // Given array
+
+const onlyOdd = (array) => {
+  let newArr = []
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] % 2 !== 0) {
+      newArr.push(array[i])
+    }
+  }
+  return newArr
+}
+
+console.log(onlyOdd(arr2))
 // output: [7, 3, 5, 13]
 
 
