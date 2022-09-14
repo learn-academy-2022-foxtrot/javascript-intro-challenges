e0b55803bfb5d051e642ec081837ab6659b6505e
rhodesmt <mrhodes415@gmail.com>
2022-09-13 17:11:19 -0700

    Conditionals Challenges

---
+++ javascript-conditionals-kl-mr.js
@@ -0,0 +1,52 @@
+// Question 1
+let item1 = 50
+
+if(item1 <= 100){
+    console.log("In Budget")
+} else {
+    console.log("Not In Budget")
+}
+
+// Question 2
+let feeling1 = "Hungry"
+let feeling2 = "Not Hungry"
+
+if (feeling1) {
+    console.log("Eat Food")
+} else {
+    console.log("Keep Coding")
+}
+
+if (feeling2) {
+    console.log("Keep Coding")
+} else {
+    console.log("Eat Food")
+} 
+
+// Question 3
+
+let trafficLight = "yellow"
+
+if(trafficLight === "green"){
+    console.log("Go")
+} else if (trafficLight === "yellow"){
+    console.log("Slow Down")
+} else if (trafficLight === "red"){
+    console.log("Stop")
+} else {
+    console.log("Broken")
+}
+// Question 4
+
+var number1 = 1
+var number2 = 10
+
+if (number1.valueOf < number2.valueOf) {
+    console.log(`${number2}`)
+} else if (number2.valueOf > number1.valueOf) {
+    console.log (`${number2}`)
+} else if (number1.valueOf === number2.valueOf) {
+    console.log("The numbers are the same.")
+}
+
+
